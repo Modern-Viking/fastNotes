cf0003c907c097383d3e4407a32e321ea66335fb
Modern Viking <thomascustoms715@gmail.com>
2020-02-10 01:10:05 -0700

    html routes working

--- backend/routes/htmlRoutes.js
+++ backend/routes/htmlRoutes.js
@@ -1,3 +1,20 @@
-const express = require("express");
 const path = require("path");
 const fs = require("fs");
+
+module.exports = function(app) {
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../frontend/index.html"));
+  });
+  
+  app.get("/assets/frontendAssets/style/styles.css", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../assets/frontendAssets/style/styles.css"));
+  });
+  
+  app.get("/assets/frontendAssets/js/index.js", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../assets/frontendAssets/js/index.js"));
+  });
+  
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../frontend/notes.html"));
+  });
+};
--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/frontend"));
 
+// require("/backend/routes/apiRoutes")(app);
+require("./backend/routes/htmlRoutes")(app);
+
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
