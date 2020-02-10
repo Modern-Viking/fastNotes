ccc616998988f3e9da83b844e315e0abd0cf21cc
Modern Viking <thomascustoms715@gmail.com>
2020-02-10 00:51:55 -0700

    server working

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/frontend"));
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
